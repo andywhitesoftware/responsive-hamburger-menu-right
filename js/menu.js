819bd4f10e9f82141bf58413a36a51cf3294209a
Andy White <andy.white504@gmail.com>
2019-05-03 09:21:21 -0400

    First Commit

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+function openHamburgerMenu() {
+  document.getElementById("right-nav").style.width = "100%";
+}
+
+function closeHamburgerMenu() {
+  document.getElementById("right-nav").style.width = "0%";
+}
